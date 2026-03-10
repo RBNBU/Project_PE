dc044543e6c4b0936c1ee15ff9c74f8ededefb3e
RBNBU <rubenbuelens@gmail.com>
2026-03-10 10:13:36 +0100

    default template for constants C and Callsigns.txt file

--- C-Code/APRS-Receive.c
+++ C-Code/APRS-Receive.c
@@ -5,18 +5,24 @@
 
 //custom libraries
 #include <mosquitto.h>
-#include <rtl-sdr.h>
 
 
 //define constants
 //RTL SDR constants
 #define APRS_FREQ 144800000 //144,800MHz (European APRS Frequency)
 #define SDR_SAMPLE_RATE 2048000 //2.048MSPS (Good for APRS)
+#define CALLSIGN_FILE "./Callsigns.txt"
 
 //MQTT constants
+//Sub / Raw
+#define MQTT_SUB "localhost"
+#define MQTT_SUB_PORT 1883
+#define MQTT_SUB_TOPIC "RTL_SDR/RAW/APRS"
+
+//Host / Filtered
 #define MQTT_HOST "localhost"
-#define MQTT_PORT 1883
-#define MQTT_TOPIC "RTL_SDR/APRS"
+#define MQTT_HOST_PORT 1883
+#define MQTT_HOST_TOPIC "RTL_SDR/FILTERED/APRS"
 
 
 int main()
